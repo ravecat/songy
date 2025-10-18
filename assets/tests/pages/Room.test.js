c38fb1f9166f78927fbea3e53d78994b7fe74025
Max Sharov <maxsharoff@gmail.com>
2025-10-19 01:41:35 +0200

    test: update spotify provider component cases

--- assets/tests/pages/Room.test.js
+++ assets/tests/pages/Room.test.js
@@ -1,10 +1,7 @@
 import { render, screen } from "@testing-library/svelte";
 import { afterEach, expect, test, vi } from "vitest";
 import Room from "~pages/Room.svelte";
-import Spotify from "~mocks/spotify";
 import socket from "~/socket";
-
-vi.stubGlobal("Spotify", Spotify);
 
 vi.mock("~/socket", async () => {
   const { Socket } = await vi.importMock("phoenix");
@@ -34,53 +31,5 @@ describe("Room", () => {
       "state_updated",
       expect.any(Function)
     );
-  });
-
-  test.skip("should initialize Spotify player when component mounts", () => {
-    render(Room, { roomId: "test-room" });
-
-    window.onSpotifyWebPlaybackSDKReady();
-
-    expect(window.Spotify.Player).toHaveBeenCalledWith({
-      name: "Songy Player test-room",
-      getOAuthToken: expect.any(Function),
-      volume: 0.5,
-    });
-  });
-
-  test("should call channel.push('get_provider') when getOAuthToken is invoked", () => {
-    render(Room, { roomId: "test-room" });
-
-    const channel = socket.channel.mock.results[0].value;
-
-    window.onSpotifyWebPlaybackSDKReady();
-
-    const player = window.Spotify.Player.mock.results[0].value;
-    const getOAuthTokenCallback = player.options.getOAuthToken;
-    const tokenCallback = vi.fn();
-
-    getOAuthTokenCallback(tokenCallback);
-
-    expect(channel.push).toHaveBeenCalledWith("get_provider", {});
-  });
-
-  test("should call channel.push('update_provider') when player ready event is fired", () => {
-    render(Room, { roomId: "test-room" });
-
-    const channel = socket.channel.mock.results[0].value;
-
-    window.onSpotifyWebPlaybackSDKReady();
-
-    const player = window.Spotify.Player.mock.results[0].value;
-
-    const [_, readyCb] = player.addListener.mock.calls.find(
-      ([event]) => event === "ready"
-    );
-
-    readyCb({ device_id: "test-device-id" });
-
-    expect(channel.push).toHaveBeenCalledWith("update_provider", {
-      device_id: "test-device-id",
-    });
   });
 });
