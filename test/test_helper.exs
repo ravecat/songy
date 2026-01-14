d2b9c27b64038cd3a65aa52f915b86de02a62237
Max Sharov <maxsharoff@gmail.com>
2026-01-14 23:43:45 +0100

    test: update outdated test environment

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,7 +2,4 @@ ExUnit.start()
 # Database connection disabled - uncomment if needed
 # Ecto.Adapters.SQL.Sandbox.mode(Songy.Repo, :manual)
 
-# Load StreamData for property-based testing with TypeCheck
-Code.ensure_loaded(StreamData)
-
 Repatch.setup()
