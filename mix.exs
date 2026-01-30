11d09ddd1479d4b9485b1932fbe95d2da5692612
Max Sharov <maxsharoff@gmail.com>
2026-01-30 12:23:05 +0100

    chore(deps): update repatch from 1.5 to 1.6.1

--- mix.exs
+++ mix.exs
@@ -66,7 +66,7 @@ defmodule Songy.MixProject do
       {:typed_struct, "~> 0.3.0"},
       {:nimble_options, "~> 1.1"},
       {:gen_state_machine, "~> 3.0"},
-      {:repatch, "~> 1.5", only: :test},
+      {:repatch, "~> 1.6", only: :test},
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
       {:recode, "~> 0.8.0", only: [:dev, :test], runtime: false},
       {:unique_names_generator, "~> 0.2"},
