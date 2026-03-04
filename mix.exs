51a71cfc6a0483ce57542b3fefaa28b003134e1f
Max Sharov <maxsharoff@gmail.com>
2026-03-04 02:13:41 +0100

    test(e2e): add fast e2e environment for local development

--- mix.exs
+++ mix.exs
@@ -106,6 +106,7 @@ defmodule Songy.MixProject do
       "assets.test.coverage": ["bun assets run test:coverage"],
       "assets.test.typecheck": ["bun assets run typecheck"],
       e2e: ["bun assets run e2e:run"],
+      "e2e.fast": ["bun assets run e2e:fast"],
       "e2e.setup": ["bun assets run e2e:install"],
       "e2e.ui": ["bun assets run e2e:ui"],
       "e2e.watch": ["bun assets run e2e:watch"],
